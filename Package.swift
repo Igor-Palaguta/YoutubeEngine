9b85d7f8b82e1b033b08127656ea40e944bfcd19
Igor Palaguta <igor.palaguta@gmail.com>
2017-10-10 11:21:02 +0300

    update manifest

--- Package.swift
+++ Package.swift
@@ -3,11 +3,9 @@ import PackageDescription
 let package = Package(
    name: "YoutubeEngine",
    dependencies: [
-      .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(3,0,0)...Version(3,1,3)),
-      .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1),
-      //.Package(url: "https://github.com/Quick/Quick", majorVersion: 1),
-      //.Package(url: "https://github.com/Quick/Nimble", majorVersion: 5, minor: 1),
-   ],
+      .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", "3.1.4"),
+      .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 2)
+   ]
    //OHHTTPStubs does not support SPM
    //exclude: ["Tests/YoutubeEngineTests/EngineSpec.swift", "Tests/YoutubeEngineTests/Responses"]
 )
